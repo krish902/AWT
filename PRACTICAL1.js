13b390faaf547bd11bc12d14fea58ed3a96731a9
Krish Bhikadiya <113178795+krish902@users.noreply.github.com>
2023-07-26 18:34:48 +0530

    Create PRACTICAL1.js

---
+++ PRACTICAL1.js
@@ -0,0 +1,12 @@
+let number = 30;
+let str = "Krish";
+let Bool = true;
+console.log(number);
+console.log(str);
+console.log(Bool);
+
+function sum(n1, n2) {
+  let ans = n1 + n2;
+  console.log("Sum : " + ans);
+}
+sum(10, 20);
